87eb16e9fe0946de9dc9ca886c2736959d2cb6ed
huynhson <danghuynhsonpys@gmail.com>
2018-01-06 14:11:14 +0700

    code form phong

---
+++ QuanLyKhachSan/QuanLyKhachSan/Phong.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//using System.Windows.SqlClient;
+
+namespace QuanLyKhachSan
+{
+    public partial class Phong : Form
+    {
+        public Phong()
+        {
+            InitializeComponent();
+        }
+        public static int maphong;
+        CauLenh cl = new CauLenh();
+        DataTable dt = new DataTable();
+        //SqlDataReader dr;
+        private void load()
+        {
+            bt_capnhat.Enabled = false;
+            bt_tinhtien.Enabled = false;
+            if(rd_kt.Checked== true)
+            {
+                bt_capnhat.Enabled = true;
+                bt_tinhtien.Enabled = true;
+            }
+            else
+            {
+                bt_capnhat.Enabled = false;
+                bt_tinhtien.Enabled = false;
+            }
+        }
+        private void Phong_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
