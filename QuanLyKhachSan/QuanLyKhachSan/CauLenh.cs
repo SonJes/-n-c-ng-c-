739159d497299d956a6cbafbd0d3bad7f9b269c6
huynhson <danghuynhsonpys@gmail.com>
2018-01-06 13:39:46 +0700

    thay doi folder

---
+++ QuanLyKhachSan/QuanLyKhachSan/CauLenh.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKhachSan
+{
+    class CauLenh
+    {
+        SqlConnection con = new SqlConnection(@"Data Source = JOHAN\SQLEXPRESS; Initial Catalog = Qlks; Integrated Security = True");
+        DataTable dt = new DataTable();
+    }
+    public DataTable dangnhap(string tk, string mk)
+    {
+        if (con.State == ConnectionState.Open)
+            con.Close();
+        con.Open();
+        SqlCommand cm = new SqlCommand("dangnhap", con);
+        cm.CommandType = CommandType.StoredProcedure;
+        cm.Parameters.AddWithValue("@taikhoan", tk);
+        cm.Parameters.AddWithValue("@matkhau", mk);
+        SqlDataAdapter da = new SqlDataAdapter(cm);
+        da.Fill(dt);
+        return dt;
+    }
+}
