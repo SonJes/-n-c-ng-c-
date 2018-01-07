36b6960930d0ce1603d6cee4aa57dd8cc3e0cc50
huynhson <danghuynhsonpys@gmail.com>
2018-01-07 13:25:18 +0700

    code moi

--- QuanLyKhachSan/QuanLyKhachSan/DoanhThu.cs
+++ QuanLyKhachSan/QuanLyKhachSan/DoanhThu.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace QuanLyKhachSan
 {
@@ -15,6 +16,29 @@ namespace QuanLyKhachSan
         public DoanhThu()
         {
             InitializeComponent();
+        }
+        CauLenh cl = new CauLenh();
+        DataTable dt = new DataTable();
+        private double tt = 0;
+        private void load()
+        {
+            txt_mp.Enabled = false;
+            txt_n1.Enabled = false;
+            txt_n2.Enabled = false;
+            txt_t.Enabled = false;
+            dt = cl.Doanhthu(0);
+            dg.DataSource = dt;
+            tt = 0;
+            for (int i = 0; i < dg.RowCount; i++)
+            {
+                tt += Convert.ToDouble(dg.Rows[i].Cells["Tienphong"].Value);
+            }
+            lb_dt.Text = tt.ToString();
+
+        }
+        private void Doanhthu_Load(object sender, EventArgs e)
+        {
+            load();
         }
     }
 }
